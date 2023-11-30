3a41d03ed725522753b159efaea9b266e0fc9e27
Mlooney <43999882+SlavaDuev@users.noreply.github.com>
2023-11-30 19:52:51 +0300

    Create init in code.cs

---
+++ code.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<string> array1 = new List<string> { "abc", "abcde", "55", "adsfa", "321" };
+    static List<string> array2 = new List<string>();
+}
